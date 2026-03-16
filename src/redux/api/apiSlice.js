1544b6285898e101ebdac83c7abb54c3242e453d
Tidjani Bachir <Tidjani_Bachir@etu.univ-blida.dz>
2026-03-16 05:59:05 +0100

    add Bearer token to request headers

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,7 +1,17 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL,credentials: 'include', });
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: BASE_URL,
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.userInfo?.token;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
